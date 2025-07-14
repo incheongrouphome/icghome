72b117ecefaba2fe2abdc722df413d2c8b9ca8c2
bugerboy1230 <bugerboy1230@naver.com>
2025-07-14 17:15:59 +0900

    fix: ES 모듈 import 오류 해결 - 파일 확장자 명시

--- server/index.ts
+++ server/index.ts
@@ -2,8 +2,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes";
-import { setupVite, serveStatic, log } from "./vite";
+import { registerRoutes } from "./routes.js";
+import { setupVite, serveStatic, log } from "./vite.js";
 
 const app = express();
 app.use(express.json());
